922dfb49f8989e59844d626420b0f9e373414b36
Peter Schroeter <peter.schroeter@rightscale.com>
2019-02-12 10:41:12 -0800

    PHX-1467 Add version script + note

---
+++ version.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+# use GNU coreutils sort because it supports version sort
+case `uname` in
+(Darwin)
+  sort='gsort'
+  ;;
+(*)
+  sort='sort'
+  ;;
+esac
+
+# collect all the latest versions tagged in Git for each major version
+declare -A versions
+while read version; do
+  if [[ $version =~ ^v([0-9]+)\.[0-9]+\.[0-9]+$ ]]; then
+    versions[${BASH_REMATCH[1]}]=$version
+  fi
+done < <(git tag -l 'v*' | $sort --version-sort)
+
+# output YAML with top level "versions" containing a dictionary of the major version numbers as keys and latest versions
+# as values
+cat <<EOF
+# Latest right_pt versions by major version (this file is used by right_pt's update check mechanism)
+---
+versions:
+EOF
+for major in ${!versions[@]}; do
+  # only output versions 1 and above
+  if [[ $major -ge 1 ]]; then
+    echo "  $major: ${versions[$major]}"
+  fi
+done
